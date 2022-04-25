20bd84925fc651b8804835b44066706470848165
hossamAlsankary <hossamAlsankary@gmail.com>
2022-04-25 17:17:44 +0200

    add ui

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require("express"),
 //Initializing Importint Modules
 require("dotenv").config();
 require("express-async-errors");
+var morgan = require('morgan');
+//set ejs
+app.set('view engine', 'ejs');
 
 //Import Custom Modules
 const updataAsyncData = require("./serves/updataeverything"),
@@ -18,14 +21,26 @@ const updataAsyncData = require("./serves/updataeverything"),
 
 //Careate New Express applications and Configure It
 app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', function (req, res) {
-let api = "/api/v1/magicano";
-res.send('<h1>Magcano</h1><a href="/api/v1/magicano">Documentation</a>');});
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+app.get('/singin', function(req, res) {
+  res.render('pages/singin');
+});
+app.get("/main" , function(req ,res){
+res.render("pages/main");
+});
+app.get("/expect" , function(req ,res){
+  res.render("pages/expect");
+  });
+  
+
 //Configure Routes
 app.use("/api/v1/magicano", event);
 app.use("/api/v1/auth", auth);
-
+morgan('tiny');
 //Config MiddleWare
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -34,9 +49,9 @@ app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
-    await connectDB(process.env.DATABASE_URL);
-    //await connectDB(config.getDBString());
-    updataAsyncData();
+    //await connectDB(process.env.DATABASE_URL);
+    await connectDB(config.getDBString());
+   // updataAsyncData();
     app.listen(config.PORT, () =>
       console.log(config.getSereverUrl(), "API Listen", config.getMagicanoUrl())
     );
